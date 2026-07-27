f262f5cf71996e9486d25d43898776d214bc7505
Deepanshu Singh <deepanshu.s@celestialsys.com>
2026-07-27 23:35:56 +0530

    addFeature

--- Backend/prisma/seed.ts
+++ Backend/prisma/seed.ts
@@ -1,8 +1,7 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../generated/prisma/client";
-
-import { seedRoles } from "./roleSeed";
+import { PrismaClient,Role, RestaurantStatus } from "../generated/prisma/client";
+import bcrypt from "bcryptjs";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
@@ -11,12 +10,166 @@ const adapter = new PrismaPg({
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
-  console.log("Seeding database...");
+  const password = await bcrypt.hash("adm123@&", 10);
 
-  await seedRoles(prisma);
-//   await seedAdmin(prisma);
 
-  console.log("Database seeded successfully!");
+  // =====================
+  // ADMIN
+  // =====================
+
+  const admin = await prisma.user.create({
+    data: {
+      firstname: "Super",
+      lastname: "Admin",
+      email: "admin@gmail.com",
+      phone: "9999999999",
+      password,
+      role: Role.ADMIN,
+      phoneVerified: true,
+    },
+  });
+
+
+  // =====================
+  // RESTAURANT OWNERS
+  // =====================
+
+  const owner1 = await prisma.user.create({
+    data: {
+      firstname: "Rahul",
+      lastname: "Sharma",
+      email: "rahul@gmail.com",
+      phone: "rah123@&",
+      password,
+      role: Role.OWNER,
+      phoneVerified: true,
+    },
+  });
+
+
+  const owner2 = await prisma.user.create({
+    data: {
+      firstname: "Amit",
+      lastname: "Verma",
+      email: "amit@gmail.com",
+      phone: "7777777777",
+      password,
+      role: Role.OWNER,
+      phoneVerified: true,
+    },
+  });
+
+
+
+  // =====================
+  // RESTAURANTS
+  // =====================
+
+
+  const spiceGarden = await prisma.restaurant.create({
+    data: {
+      restaurantName: "Spice Garden",
+      cuisine: "North Indian",
+      address: "MG Road, Bangalore",
+      phone: "8888888888",
+      fssaiNumber: "12345678901234",
+      gstNumber: "29ABCDE1234F1Z5",
+
+      status: RestaurantStatus.APPROVED,
+
+      ownerId: owner1.id,
+
+      approvedBy: admin.id,
+      approvedAt: new Date(),
+      adminNote: "Verified restaurant",
+    },
+  });
+
+
+  const burgerHub = await prisma.restaurant.create({
+    data: {
+      restaurantName: "Burger Hub",
+      cuisine: "Fast Food",
+      address: "Koramangala, Bangalore",
+      phone: "7777777777",
+
+      status: RestaurantStatus.APPROVED,
+
+      ownerId: owner2.id,
+
+      approvedBy: admin.id,
+      approvedAt: new Date(),
+    },
+  });
+
+
+
+  // =====================
+  // MENU ITEMS
+  // =====================
+
+
+  await prisma.menuItem.createMany({
+    data: [
+      {
+        name: "Chicken Biryani",
+        description: "Hyderabadi style spicy chicken biryani",
+        price: 220,
+        category: "Biryani",
+        isVeg: false,
+        imageUrl: "https://res.cloudinary.com/delubzbh2/image/upload/v1784400937/FoodDelivery/jmgmhpz0e9vvobusyf6k.jpg",
+        restaurantId: spiceGarden.id,
+      },
+
+      {
+        name: "Paneer Butter Masala",
+        description: "Creamy paneer curry",
+        price: 180,
+        category: "Main Course",
+        isVeg: true,
+        imageUrl: "https://res.cloudinary.com/delubzbh2/image/upload/v1784400826/FoodDelivery/dwbm21mcxc7epr9fq7p5.jpg",
+        restaurantId: spiceGarden.id,
+      },
+
+      {
+        name: "Masala Dosa",
+        description: "South Indian crispy dosa",
+        price: 90,
+        category: "South Indian",
+        isVeg: true,
+        imageUrl: "https://res.cloudinary.com/delubzbh2/image/upload/v1785174361/FoodDelivery/cgjunwvwngqfzkxhmkhl.webp",
+
+        restaurantId: spiceGarden.id,
+      },
+
+      {
+        name: "Classic Cheese Burger",
+        description: "Loaded cheese burger",
+        price: 150,
+        category: "Burger",
+        isVeg: true,
+        imageUrl: "https://res.cloudinary.com/delubzbh2/image/upload/v1785174417/FoodDelivery/vt6cb4cyadkat8tcjoi8.jpg",
+
+        restaurantId: burgerHub.id,
+      },
+
+      {
+        name: "Chicken Burger",
+        description: "Grilled chicken burger",
+        price: 220,
+        category: "Burger",
+        isVeg: false,
+        imageUrl:
+          "https://res.cloudinary.com/delubzbh2/image/upload/v1785174485/FoodDelivery/rt7mosg3ye1nqb4akxmf.jpg",
+
+        restaurantId: burgerHub.id,
+      },
+    ],
+  });
+
+
+
+  console.log("Database seeded successfully");
 }
 
 main()
